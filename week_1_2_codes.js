137abcdf904e4b4b22a95145276392260315c3ae
srijan karak <srijankarak2001@gmail.com>
2025-11-15 23:26:55 +0530

    second commit

--- week_1_2_codes.js
+++ week_1_2_codes.js
@@ -4,17 +4,37 @@ const usersname =["srijan","sayan","rohan","ayan"];
 const allUsers =[{
     firstName:"srijan",
     lastName:"karak",
+    gender:"male",
     age:24
 },{
     firstName:"sayan",
     lastName:"chakraborty",
+    gender:"male",
     age:23  
 },{
     firstName:"rohan",
     lastName:"das",
+    gender:"male",
     age:25
 },{
     firstName:"ayan",
-    lastName:"gupta",
+    lastName:"gupta",   
+    gender:"male",
     age:22
+},{
+    firstName:"sagarika",
+    lastName:"mukherjee",       
+    gender:"female",
+    age:24  
+},{
+    firstName:"smriti",
+    lastName:"roy",
+    gender:"female",    
+    age:23
 }];
+
+for(let i=0;i<allUsers.length;i++){
+    if(allUsers[i].gender==="male"){
+        console.log(`Mr. ${allUsers[i].firstName} ${allUsers[i].lastName}, Age: ${allUsers[i].age}`);
+    }
+}
