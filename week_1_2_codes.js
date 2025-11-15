167e7528944033d73476d214c59de75dfd353f1c
srijan karak <srijankarak2001@gmail.com>
2025-11-15 23:21:27 +0530

    first commit

---
+++ week_1_2_codes.js
@@ -0,0 +1,20 @@
+const users =[1 , 2 , 3 , 4];
+const usersname =["srijan","sayan","rohan","ayan"];
+
+const allUsers =[{
+    firstName:"srijan",
+    lastName:"karak",
+    age:24
+},{
+    firstName:"sayan",
+    lastName:"chakraborty",
+    age:23  
+},{
+    firstName:"rohan",
+    lastName:"das",
+    age:25
+},{
+    firstName:"ayan",
+    lastName:"gupta",
+    age:22
+}];
