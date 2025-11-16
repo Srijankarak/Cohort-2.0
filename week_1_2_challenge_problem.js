542b4e2cd4610da8682a7c33da1ef5682c16056f
srijan karak <srijankarak2001@gmail.com>
2025-11-16 12:42:09 +0530

    another solution

--- week_1_2_challenge_problem.js
+++ week_1_2_challenge_problem.js
@@ -18,3 +18,14 @@ displayResult(ans);
 
 // but the challeng is that you have to do with a single function call only
 displayResult(sum(1,2));
+// another solution is 
+function sum2(nums1, nums2 , fnTOcall){
+    let result = nums1 + nums2;
+    fnTOcall(result);
+}
+
+function fncall(data){
+    console.log("The data received is: " + data);
+
+}
+sum2(1,2,fncall);
