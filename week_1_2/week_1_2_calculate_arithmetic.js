3e1ba30d4775cea9a3c84b427cf787d3d0e5ee93
srijan karak <srijankarak2001@gmail.com>
2025-11-16 21:51:38 +0530

    updated

---
+++ week_1_2/week_1_2_calculate_arithmetic.js
@@ -0,0 +1,11 @@
+function calculateArithmetic(a , b , ){
+    //
+
+}
+
+const value = calculateArithmetic(1,2 ,"minus");
+console.log(value);
+
+function sum(a,b){
+    //
+}
---
+++ week_1_2/week_1_2_settimeout.js
@@ -0,0 +1,5 @@
+function greet(){
+    console.log("Hello, world!");
+}
+console.log("hi there");
+setTimeout(greet, 2000);
