4d732f310f47cc585880b87f1079c52b3e4f9b1c
srijan karak <srijankarak2001@gmail.com>
2025-11-18 00:00:25 +0530

    Javascript functions

---
+++ week_1_3/index.js
@@ -0,0 +1,7 @@
+function findIndexOf(str , target){
+    console.log("Original String:",str);
+    console.log("Index:",str.indexOf(target));
+    console.log("Last Index:",str.lastIndexOf(target));
+}
+
+findIndexOf("hello world world" , "world");
