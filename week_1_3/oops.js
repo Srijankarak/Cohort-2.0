743d45dfac8de06b4837dbcebc2693bcf98a9af8
srijan karak <srijankarak2001@gmail.com>
2025-11-25 22:44:38 +0530

    oops concept

---
+++ week_1_3/oops.js
@@ -0,0 +1,36 @@
+//legCount , speak , name
+class Animal{
+    constructor(name , legCount , speaks){ 
+        this.name = name;
+        this.legCount = legCount;
+        this.speaks = speaks;
+    }
+    // you can also have static fuctions
+    // these are not associated with objects , these are associated with the the class itself
+    static speak(){
+        console.log("I am a static method");
+    }
+    speak(){
+        console.log(`${this.name} says ${this.speaks}`);
+    }
+}
+
+// class , objects
+
+let dog = new Animal("doggie" , 4 , "bhow bhow");
+dog.speak();
+console.log(dog);
+
+let cat = new Animal("catty",4,"meow meow");
+cat.speak();
+console.log(cat);
+
+
+// don't do this
+// let dog1={
+//     name:"doggie",
+//     legCount:4,
+//     speaks:"bhow bhow"
+// }
+
+// console.log(dog1);
---
+++ week_1_3/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+// let dog = new Animal("doggie" , 4 , "bhow bhow");
+// dog.speak();
