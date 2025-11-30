4392703952d8ccace2de281065810c31e5b4904d
srijan karak <srijankarak2001@gmail.com>
2025-11-30 15:47:23 +0530

    code updated

--- week_1_5/fs_modules.js
+++ week_1_5/fs_modules.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
+// filesystem module
 const path = require("path");
 const filePath = path.join(__dirname, "a.txt");
-// filesystem module
 fs.readFile(filePath, "utf-8", function(err, data){
     if(err){
         console.log("Error reading file:", err);
